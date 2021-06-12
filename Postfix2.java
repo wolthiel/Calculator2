57c7d9a34a42efb8fe15bc0f50a8e011ab192b22
AstnMartin <82873818+AstnMartin@users.noreply.github.com>
2021-06-12 16:03:25 +0200

    Update Postfix2.java

--- Postfix2.java
+++ Postfix2.java
@@ -1,47 +1,46 @@
 import java.util.Scanner;
 
 public class Postfix {
-	
-	
+
 	public int evaluate(String pfx) throws Exception {
-		
-		String ffff =  infixToPostfix(pfx);
-		
+
+		String ffff = infixToPostfix(pfx);
+
 		StackAsList stackforDigits = new StackAsList();
 		int result = 0;
 		int firstdigit = 0;
 		int seconddigit = 0;
 
 		char[] c = ffff.toCharArray();
-		
-		for (char chr: c) {
-			if(Character.isDigit(chr)){
-				stackforDigits.addFirstNode(chr);	
-			} else if (!(Character.isDigit(chr))){
-				
+
+		for (char chr : c) {
+			if (Character.isDigit(chr)) {
+				stackforDigits.addFirstNode(chr);
+			} else if (!(Character.isDigit(chr))) {
+
 				firstdigit = Integer.parseInt(stackforDigits.removeFirst().toString());
 				seconddigit = Integer.parseInt(stackforDigits.removeFirst().toString());
-				
-				switch(chr) {
+
+				switch (chr) {
 				case '+':
-					result = seconddigit+firstdigit;
+					result = seconddigit + firstdigit;
 					break;
 				case '/':
-					result = seconddigit/firstdigit;
+					result = seconddigit / firstdigit;
 					break;
-				case '-':	
-					result = seconddigit-firstdigit;
+				case '-':
+					result = seconddigit - firstdigit;
 					break;
-				case '*' :
-					result = seconddigit*firstdigit;
-					break;	
+				case '*':
+					result = seconddigit * firstdigit;
+					break;
 				}
 				stackforDigits.addFirstNode(result);
-				}
+			}
 		}
 		return result;
-	}    
-	    
+	}
+
 //	public static String infixToPostfix(String exp)
 //    {
 //    	char operator = 0;
@@ -72,55 +71,55 @@ public class Postfix {
 //		return string;
 //
 //}
-	
+
 	public String infixToPostfix(String ifx) throws Exception {
 		StackAsList stackforDigits = new StackAsList();
 		char[] characters = ifx.toCharArray();
+		StackAsList.StackListIterator iterator = stackforDigits.new StackListIterator();
 		String result = "";
 		Object op = '(';
-		
-		for(char t:characters) {
-			if(Character.isDigit(t)) {
+		for (char t : characters) {
+			if (Character.isDigit(t)) {
 				result = result + t;
-			}else if(t == '(') {
+			} else if (t == '(') {
 				stackforDigits.addFirstNode(t);
-			}else if(t == ')') {
-				while(stackforDigits.peek() == op){
-					result = result + stackforDigits.peek();	
-					stackforDigits.removeFirst();
-			}
-				if(stackforDigits.peek() == op) { 
-				stackforDigits.removeFirst();
-				}
-			}
-			else if(t == '+' || t == '-' || t == '*' || t == '/') {
-				while(!(stackforDigits.peek().equals(!(highPrecedence(t)) || (lowPrecedence(t))))) {
+			} else if (t == ')') {
+				while (stackforDigits.peek() == op) {
 					result = result + stackforDigits.peek();
 					stackforDigits.removeFirst();
 				}
-				stackforDigits.addFirstNode(t);
+				if (stackforDigits.peek() == op) {
+					stackforDigits.removeFirst();
+				}
+			} else if (t == '+' || t == '-' || t == '*' || t == '/') {
+				while (iterator.hasNext()) {
+					while (!(stackforDigits.peek().equals(!(highPrecedence(t)) || (lowPrecedence(t))))) {
+						result = result + stackforDigits.peek();
+						stackforDigits.removeFirst();
+					}
+					stackforDigits.addFirstNode(t);
+				}
 			}
 		}
-		while(!stackforDigits.isEmpty()) {
+		while (!stackforDigits.isEmpty()) {
 			result = result + stackforDigits.peek();
-			stackforDigits.removeFirst();		
+			stackforDigits.removeFirst();
 		}
 		return result;
 	}
-	
+
 	private boolean highPrecedence(char c) {
 		return (c == '*' || c == '/');
 	}
-	
+
 	private boolean lowPrecedence(char c) {
 		return (c == '+' || c == '-');
 	}
-		
-		public void InputofUserInConsole() throws Exception {
-			Scanner scan = new Scanner(System.in);
-			System.out.print("Input: ");
-			String console = scan.nextLine();
-			System.out.println("Output: " + evaluate(console));
-		}
-}
 
+	public void InputofUserInConsole() throws Exception {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input: ");
+		String console = scan.nextLine();
+		System.out.println("Output: " + evaluate(console));
+	}
+}
