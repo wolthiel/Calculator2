3bd67cc465a95418800a02b8e9ee75e13114e8c8
wolthiel <82994042+wolthiel@users.noreply.github.com>
2021-06-15 11:09:56 +0200

    Jetzt fehlen nur noch die Testcases

--- calculator-full-solution/CalcEngine.java
+++ calculator-full-solution/CalcEngine.java
@@ -21,6 +21,8 @@ public class CalcEngine
     private int displayValue;
     // The value of an existing left operand.
     private int leftOperand;
+    
+    public int evaluated;
 
     /**
      * Create a CalcEngine.
@@ -119,7 +121,12 @@ public class CalcEngine
         }
         else if (Calculator.gui.getDisplay() != null)
         {
-            System.out.println(Calculator.gui.getDisplay());
+            try {
+                displayValue = evaluate(Calculator.gui.getDisplay());
+            }
+            catch(Exception e) {
+                System.out.println("An Error has occurred.");
+            }
         }
         else {
             keySequenceError();
@@ -230,4 +237,86 @@ public class CalcEngine
         // Reset everything.
         clear();
     }
+
+	public int evaluate(String pfx) throws Exception {
+
+		String ffff = infixToPostfix(pfx);
+
+		StackAsList stackforDigits = new StackAsList();
+		int result = 0;
+		int firstdigit = 0;
+		int seconddigit = 0;
+
+		char[] c = ffff.toCharArray();
+
+		for (char chr : c) {
+			if (Character.isDigit(chr)) {
+				stackforDigits.addFirstNode(chr);
+			} else if (!(Character.isDigit(chr))) {
+
+				firstdigit = Integer.parseInt(stackforDigits.removeFirst().toString());
+				seconddigit = Integer.parseInt(stackforDigits.removeFirst().toString());
+
+				switch (chr) {
+				case '+':
+					result = seconddigit + firstdigit;
+					break;
+				case '/':
+					result = seconddigit / firstdigit;
+					break;
+				case '-':
+					result = seconddigit - firstdigit;
+					break;
+				case '*':
+					result = seconddigit * firstdigit;
+					break;
+				}
+				stackforDigits.addFirstNode(result);
+			}
+		}
+		return result;
+	}
+	
+	public String infixToPostfix(String ifx) throws Exception {
+		StackAsList stackforDigits = new StackAsList();
+		char[] characters = ifx.toCharArray();
+		String result = "";
+		Object op = '(';
+		
+		for (char t : characters) {
+			if (Character.isDigit(t)) {
+				result = result + t;
+			} else if (t == '(') {
+				stackforDigits.addFirstNode(t);
+			} else if (t == ')') {
+				while (stackforDigits.peek() != op) {
+					result = result + stackforDigits.peek();
+					stackforDigits.removeFirst();
+				}
+				stackforDigits.removeFirst();
+			} else if (t == '+' || t == '-' || t == '*' || t == '/') {
+
+					while (!stackforDigits.isEmpty() && !(stackforDigits.peek() == op) && highPrecedence(t) <= highPrecedence((char) stackforDigits.peek())) {
+						result = result + stackforDigits.peekAsString();
+						stackforDigits.removeFirst();				
+						}
+					stackforDigits.addFirstNode(t);	
+				}
+			}	
+		while (!stackforDigits.isEmpty()) {
+			result = result + stackforDigits.peekAsString();
+			stackforDigits.removeFirst();
+		}
+		return result;
+	}
+
+	private int highPrecedence(char c) {
+		if(c == '*' || c == '/') {
+			return 2;
+		}
+		if(c == '+' || c == '-') {
+			return 1;
+		}
+		return 0;
+	}
 }
